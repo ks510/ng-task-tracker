6727d1e851aa8e98066aa5deff9d7e442201dcfc
Christian Schönlein <christian.schoenlein@t-online.de>
2021-08-22 02:45:51 +0200

    feat: added angular material and refactored form code

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Task } from '../../Task';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -10,39 +11,54 @@ import { Subscription } from 'rxjs';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
-  text: string;
-  day: string;
-  reminder: boolean = false;
   showAddTask: boolean;
   subscription: Subscription;
+  form: FormGroup;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private readonly fb: FormBuilder) {
     this.subscription = this.uiService
     .onToggle()
     .subscribe((value) => (this.showAddTask = value));
   }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      text: ['', Validators.required],
+      day: [''],
+      reminder: false
+    })
+  }
+
+  get text(): AbstractControl {
+    return this.form.get('text');
+  }
+
+  get day(): AbstractControl {
+    return this.form.get('day');
+  }
+
+  get reminder(): AbstractControl {
+    return this.form.get('reminder');
   }
 
   onSubmit() {
-    if (!this.text) {
+    if (!this.text.value) {
       alert('Please add a task!');
       return;
     }
 
     const newTask = {
-      text: this.text,
-      day_time: this.day,
-      reminder: this.reminder
+      text: this.text.value,
+      day_time: this.day.value,
+      reminder: this.reminder.value
     }
 
     // @todo - emit event
     this.onAddTask.emit(newTask);
-
-    this.text = '';
-    this.day = '';
-    this.reminder = false;
+    this.initForm()
   }
-
 }
