c4ec3b7414daab2ad764aeb70873a543cf48241d
Karen <ksuen.dev@gmail.com>
2021-08-24 01:29:52 +0100

    feat: added form to dialog

--- src/app/components/create-task/create-task.component.ts
+++ src/app/components/create-task/create-task.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
-  styleUrls: ['./create-task.component.css']
+  styleUrls: ['./create-task.component.scss']
 })
 export class CreateTaskComponent implements OnInit {
 
-  constructor() { }
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  showAddTask: boolean;
+  subscription: Subscription;
+  form: FormGroup;
+
+  constructor(private uiService: UiService, private readonly fb: FormBuilder) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      text: ['', Validators.required],
+      day: [''],
+      reminder: false
+    })
+  }
+
+  get text(): AbstractControl {
+    return this.form.get('text');
+  }
+
+  get day(): AbstractControl {
+    return this.form.get('day');
+  }
+
+  get reminder(): AbstractControl {
+    return this.form.get('reminder');
+  }
+
+  onSubmit() {
+    if (!this.text.value) {
+      alert('Please add a task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text.value,
+      day_time: this.day.value,
+      reminder: this.reminder.value
+    }
+
+    // @todo - emit event
+    this.onAddTask.emit(newTask);
+    this.initForm()
   }
 
 }
